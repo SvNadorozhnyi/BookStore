ecc2f28e9db0d982e9d94605372b9c4d0d5594e4
Svyatoslav Nadorozhnyi <svyatoslavnadorozhniy@gmail.com>
2024-10-15 15:27:45 -0400

    made all the requested changed

--- src/main/java/org/example/bookstore/service/impl/BookServiceImpl.java
+++ src/main/java/org/example/bookstore/service/impl/BookServiceImpl.java
@@ -1,16 +1,16 @@
 package org.example.bookstore.service.impl;
 
 import java.util.List;
+import lombok.RequiredArgsConstructor;
 import org.example.bookstore.model.Book;
 import org.example.bookstore.repository.BookRepository;
 import org.example.bookstore.service.BookService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class BookServiceImpl implements BookService {
-    @Autowired
-    private BookRepository bookRepository;
+    private final BookRepository bookRepository;
 
     @Override
     public Book save(Book book) {
